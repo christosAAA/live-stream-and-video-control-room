3c68472e378616e08ba3ec1db983b38f526689b3
Christos Atzinas <christos.atzinas@gmail.com>
2021-04-27 01:08:26 +0200

    react-clean-console-logs1

--- docker-reactApp/src/components/AdminPage/MainContent/ControlRoom/VideoList/utils.ts
+++ docker-reactApp/src/components/AdminPage/MainContent/ControlRoom/VideoList/utils.ts
@@ -14,7 +14,6 @@ export const setSelectState = (
   }
   let selectState = document.getElementById(`button${key}`)
   if (selectState) {
-    console.log('change state')
     selectState.className = `${classes.listItem} ${classes.setActive}`
     setPrev(key)
   }
@@ -26,17 +25,13 @@ export const setDeleteIcon = (
   prev?: number,
   setPrev?: React.Dispatch<React.SetStateAction<number>>
 ) => {
-  console.log('set delete icon', key)
   if (test === 'chooseVideo') {
-    console.log('set delete icon1')
     let deleteIconButtonPrev = document.getElementById(`deleteIcon${prev}`)
-    console.log(deleteIconButtonPrev)
     if (deleteIconButtonPrev) {
       deleteIconButtonPrev.style.display = 'none'
     }
     if (key === 0) return
     let deleteIconButton = document.getElementById(`deleteIcon${key}`)
-    console.log(deleteIconButton)
     if (deleteIconButton) {
       deleteIconButton.style.display = 'inline-block'
       if (setPrev) {
@@ -53,13 +48,6 @@ export const setDeleteIcon = (
 }
 
 export const setSpinnerIcon = (enable: boolean, key: number) => {
-  // if (!key) {
-  //   let spinnerIconAll = document.querySelectorAll("[id^=spinner]");
-  //   spinnerIconAll.forEach((key) => {
-  //     let icon = document.getElementById(`spinner${key}`);
-  //     if (icon) icon.style.display = "none";
-  //   });
-  // } else {
   let spinnerIcon = document.getElementById(`spinner${key}`)
   if (spinnerIcon) {
     if (enable === true) {
