4225986ccdc7f5daf4e5347e317aa9a26593b3e4
Christos Atzinas <christos.atzinas@gmail.com>
2021-05-04 20:18:40 +0200

    js-file-clear

--- docker-expressApp/dist/src/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.uploadPath = exports.path = exports.port = void 0;
-exports.port = process.env["NODE_ENV"] === "development" ? 5000 : 80;
-exports.path = process.env["NODE_ENV"] === "development" ? 'dist/src/api/' : '/app/dist/src/api/';
-exports.uploadPath = process.env["NODE_ENV"] === "development" ? 'dist/src/uploads/' : '/app/dist/src/uploads/';
